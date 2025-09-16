f03beba88c6c25b0bfc73ce6e04fe51d97dde6b7
mohit tailor <mohittailordeveloper@gmail.com>
2025-09-16 08:26:55 +0530

    try to increase the spped of the app

--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,33 @@ import WelcomeComponent from "@/components/WelcomeComponent";
 import OurCoreServices from "@/components/OurCoreServices";
 
 const FAQs = dynamic(() => import("@/components/FAQ/FAQs"), {
-  ssr: false,
   loading: () => <p>Loading…</p>,
 });
 const IndustriesWeServe = dynamic(
   () => import("@/components/IndustriesWeServe"),
   {
-    ssr: false,
     loading: () => <p>Loading…</p>,
   }
 );
 const ITSupportInsights = dynamic(
   () => import("@/components/ITSupportInsights"),
   {
-    ssr: false,
     loading: () => <p>Loading…</p>,
   }
 );
 const OurCommitment = dynamic(() => import("@/components/OurCommitment"), {
-  ssr: false,
   loading: () => <p>Loading…</p>,
 });
 const OurGlobalPresence = dynamic(
   () => import("@/components/OurGlobalPresence"),
   {
-    ssr: false,
     loading: () => <p>Loading…</p>,
   }
 );
 const WhyChoose = dynamic(() => import("@/components/WhyChoose"), {
-  ssr: false,
   loading: () => <p>Loading…</p>,
 });
 const ScrollToTop = dynamic(() => import("@/components/ScrollToTop"), {
-  ssr: false,
   loading: () => <p>Loading…</p>,
 });
 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
+  swcMinify: true,
   experimental: {
+    optimizePackageImports: ["react", "react-dom"],
     turbo: {
       rules: {
         "*.svg": {
@@ -23,4 +30,5 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
+
